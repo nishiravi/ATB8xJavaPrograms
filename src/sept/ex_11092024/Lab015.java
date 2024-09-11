f2941f0b420e087d332b289ee6c2f45e806fa1f0
nishi <r.nishi61@gmail.com>
2024-09-10 22:22:18 -0400

    my first push

--- src/sept/ex_11092024/Lab015.java
+++ src/sept/ex_11092024/Lab015.java
@@ -1,4 +1,14 @@
 package sept.ex_11092024;
 
 public class Lab015 {
+    public static void main(String[] args) {
+
+        byte a=10;
+        short b=128;
+        int c=100;
+        long d=14141414141l;
+    }
 }
+
+
+
