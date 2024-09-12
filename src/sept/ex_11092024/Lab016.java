2e1b4a2119418f051e66f879fbe4da1955adfb83
nishi <r.nishi61@gmail.com>
2024-09-12 11:39:35 -0400

    my first push

---
+++ src/sept/ex_11092024/Lab016.java
@@ -0,0 +1,17 @@
+package sept.ex_11092024;
+
+public class Lab016 {
+
+    public static void main(String[] args) {
+
+        char a='A';
+        System.out.println(a);
+
+        char b='$';
+        System.out.println(b);
+
+
+        char c='\n';
+        System.out.println(c);
+    }
+}
