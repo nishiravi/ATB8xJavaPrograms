679a95a7176fbb6ef7940bad9d057c74063f1d28
nishi <r.nishi61@gmail.com>
2024-09-12 22:21:11 -0400

    my first push

---
+++ src/sept/ex_13092024/Lab019.java
@@ -0,0 +1,8 @@
+package sept.ex_13092024;
+
+public class Lab019 {
+    public static void main(String[] args) {
+        System.out.println(" hi nishi");
+        System.out.print("hi");
+    }
+}
---
+++ src/sept/ex_13092024/Lab020_PrintTable.java
@@ -0,0 +1,18 @@
+package sept.ex_13092024;
+
+public class Lab020_PrintTable {
+    public static void main(String[] args) {
+ // print the table of 2;
+        // formatting
+        //%d= integer,byte,short,long
+        //%s=String
+        //%c=char
+        //%f=float, double
+        int num=10;
+        for (int i=1;i<=10;i++)
+        {
+            System.out.printf("%d * %d=%d",num,i,num*i);
+            System.out.println();
+        }
+    }
+}
