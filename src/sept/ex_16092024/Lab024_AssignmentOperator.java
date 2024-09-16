1daf966b890e0556210ff89b8a6f86041ae35f91
nishi <r.nishi61@gmail.com>
2024-09-16 10:51:57 -0400

    my first push

---
+++ src/sept/ex_16092024/Lab024_AssignmentOperator.java
@@ -0,0 +1,9 @@
+package sept.ex_16092024;
+
+public class Lab024_AssignmentOperator {
+    public static void main(String[] args) {
+        int age=35;
+
+
+    }
+}
---
+++ src/sept/ex_16092024/Lab025_Arithmetic_Operator.java
@@ -0,0 +1,16 @@
+package sept.ex_16092024;
+
+public class Lab025_Arithmetic_Operator
+{
+    public static void main(String[] args) {
+
+        //Arthimetic operators
+        //+,-,*,/
+        int a=5;
+        int b=6;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
