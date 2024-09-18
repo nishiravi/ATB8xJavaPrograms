086adf66c1615e7ccf63c2f59d9e45b4119ea34e
nishi <r.nishi61@gmail.com>
2024-09-18 10:42:10 -0400

    my first push

---
+++ src/sept/ex_16092024/Task_16sept/AssignmentOperator.java
@@ -0,0 +1,11 @@
+package sept.ex_16092024.Task_16sept;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+
+        int a=10; // assignment operator
+        boolean b= (10==10);  // here its a relational operator
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
