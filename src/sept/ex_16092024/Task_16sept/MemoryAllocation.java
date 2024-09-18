0655476de40ce2da7efacc40d06ff2927d61deaf
nishi <r.nishi61@gmail.com>
2024-09-18 10:48:15 -0400

    my first push

---
+++ src/sept/ex_16092024/Task_16sept/MemoryAllocation.java
@@ -0,0 +1,9 @@
+package sept.ex_16092024.Task_16sept;
+
+public class MemoryAllocation {
+    public static void main(String[] args) {
+        byte b = 10; // 1 byte, 8 bits
+        long l = 10l;// 8 bytes, 64 bits
+        String s="name"; // 8 bytes,64 bits
+    }
+}
