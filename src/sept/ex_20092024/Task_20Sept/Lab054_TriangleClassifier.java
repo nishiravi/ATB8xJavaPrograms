01fc99c6b0860259263d8b48dce7f7a1781ef50b
nishi <r.nishi61@gmail.com>
2024-09-23 10:06:01 -0400

    Task 20sept pushed

--- src/sept/ex_20092024/Task_20Sept/Lab054_TriangleClassifier.java
+++ src/sept/ex_20092024/Task_20Sept/Lab054_TriangleClassifier.java
@@ -3,8 +3,8 @@ package sept.ex_20092024.Task_20Sept;
 public class Lab054_TriangleClassifier {
     public static void main(String[] args) {
         int side1=5;
-        int side2=2;
-        int side3=5;
+        int side2=1;
+        int side3=2;
         if ((side1==side2) && (side2==side3) && (side1==side3))
         {
             System.out.println(" triangle is equilateral (all sides are equal),");
