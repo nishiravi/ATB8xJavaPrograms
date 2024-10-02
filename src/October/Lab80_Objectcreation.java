6218d76492fe56e0eccdcfcede623efe703c7348
nishi <r.nishi61@gmail.com>
2024-10-02 11:57:14 -0400

    2OCTOBER Practise & Task_of_the Day Code

---
+++ src/October/Lab80_Objectcreation.java
@@ -0,0 +1,29 @@
+package October;
+
+public class Lab80_Objectcreation {
+    public static void main(String[] args) {
+        ATBStudent obj1=new ATBStudent();
+        ATBStudent obj2=new ATBStudent();
+        ATBStudent obj3=new ATBStudent();
+        ATBStudent obj4=new ATBStudent();
+        ATBStudent obj5=new ATBStudent();
+        obj1.age=10;
+        obj1.eat();
+        obj2.age=20;
+        obj2.eat();
+        obj3.age=25;
+        obj3.eat();
+        obj4.age=28;
+        obj4.eat();
+        obj5.age=30;
+        obj5.eat();
+        System.out.println("obj1 age is"+obj1.age);
+        System.out.println("obj2 age is"+obj2.age);
+        System.out.println("obj3 age is"+obj3.age);
+        System.out.println("obj4 age is"+obj4.age);
+        System.out.println("obj5 age is"+obj5.age);
+
+
+
+    }
+}
--- src/Task/MasterCoding_InterviewQuestions/PrimeNumber.java
+++ src/Task/MasterCoding_InterviewQuestions/PrimeNumber.java
@@ -6,24 +6,26 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        if (num <= 1) {
+            System.out.println("invalid data range");
+        } else {
+            for (int i = 2; i <= num; i++) {
+                boolean isPrime = true;
+                if (i > 1) {
+                    for (int j = 2; j < i; j++) {
+                        if (i % j == 0) {
+                            isPrime = false;
+                            break;
+                        }
 
-        for (int i = 1; i <= num; i++) {
-            boolean isPrime = true;
-            if (i > 1) {
-                for (int j = 2; j < i; j++) {
-                    if (i % j == 0) {
-                        isPrime = false;
-                        break;
                     }
+                    if (isPrime == true) {
+                        System.out.println(i + " is a prime number");
+                    }
+                }
 
-                }
-                if(isPrime==true)
-                {
-                    System.out.println(i+" is a prime number");
-                }
             }
-
         }
     }
-    }
+}
 
