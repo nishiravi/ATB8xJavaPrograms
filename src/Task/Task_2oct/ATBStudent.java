068f567e6af2cae91cabe2782cfb16534e7dd0a1
nishi <r.nishi61@gmail.com>
2024-10-02 11:55:48 -0400

    2OCTOBER Practise & Task_of_the Day Code

---
+++ src/Task/Task_2oct/ATBStudent.java
@@ -0,0 +1,22 @@
+package Task.Task_2oct;
+
+public class ATBStudent extends Object {
+
+    // C - ATBStudent
+    //  A - Attribute | data members | instance variables | fields | member variables
+    long phone;
+    String email;
+    int age;
+    int batch8x;
+    String name;
+    boolean isMale;
+    String address;
+    boolean isfeedPaid;
+    String githublink;
+    String sid;
+
+    //  B - Behaviour | methods | member functions
+    void study(){} // NRNA
+    void eat(){}
+    void walk(){}
+}
