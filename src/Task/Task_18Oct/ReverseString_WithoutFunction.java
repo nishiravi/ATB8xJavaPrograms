cd80e8e0d129ed736d0c5f2e974f1e2c32860ac1
nishi <r.nishi61@gmail.com>
2024-10-18 14:07:47 -0400

    18th OCTOBER Practise  Code and Task Programs

--- src/Task/Task_18Oct/ReverseString_WithoutFunction.java
+++ src/Task/Task_18Oct/ReverseString_WithoutFunction.java
@@ -2,7 +2,7 @@ package Task.Task_18Oct;
 
 public class ReverseString_WithoutFunction {
     public static void main(String[] args) {
-        String name="Nishi";
+        String name="abc";
         String a="";
        for(int i=name.length()-1;i>=0;i--)
        {
