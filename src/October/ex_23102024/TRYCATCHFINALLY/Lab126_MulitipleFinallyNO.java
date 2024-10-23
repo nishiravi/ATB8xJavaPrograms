d40955361f841552d166a885ff2f4a4f4ded5b0a
nishi <r.nishi61@gmail.com>
2024-10-23 12:33:48 -0400

    23rd OCTOBER Practise  Code

---
+++ src/October/ex_23102024/TRYCATCHFINALLY/Lab126_MulitipleFinallyNO.java
@@ -0,0 +1,20 @@
+package October.ex_23102024.TRYCATCHFINALLY;
+
+public class Lab126_MulitipleFinallyNO {
+    public static void main(String[] args) {
+
+        int a=0;
+        try{
+            int c=10/a;
+            System.out.println(c);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+           // System.out.println(e.getMessage());
+        }finally {
+            System.out.println(" Finally block executed");
+        }
+    //    finally { // Multiple Finally block not allowed
+            System.out.println(" Finally block executed");
+        }
+    }
+}
